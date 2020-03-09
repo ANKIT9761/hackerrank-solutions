a3a573c8be4bbeb7aa822d59832c2cd0a784b0bf
Ankit Kanyal <35369589+ANKIT9761@users.noreply.github.com>
2020-03-09 18:26:53 +0530

    Update Alphabet rangoli.py

--- Python/Alphabet rangoli.py
+++ Python/Alphabet rangoli.py
@@ -1,13 +1,26 @@
-n=int(input())
-s=ord('a')
-width=4*(n-1)+1
-l=[]
-count=s+n-1
-st=chr(count)
-print(st.center(width,'-'))
-for i in range(1,n):
-    rev=st[::-1]
-    a=len(st)//2
-    st=st[:a+1]+"-"+chr(count-i)+"-"+rev[:a+1]
-    print(st.center(width,'-'))
+def print_rangoli(n):
+    s=ord('a')
+    width=4*(n-1)+1
+    count=s+n-1
+    l=[]
+    st=chr(count)
+    l.append(st.center(width,'-'))
+    for i in range(1,n):
+        a=len(st)//2
+        st=st[:a+1]+"-"+chr(count-i)+"-"+st[:a+1][::-1]
+        l.append(st.center(width,'-'))
+    for i in l[::-1][1:len(l)]:
+        l.append(i)
+    for i in l:
+        print(i)
 
+
+
+
+
+        
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
